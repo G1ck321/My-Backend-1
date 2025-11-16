5b25a5be49c53587d34a8dcb098552b737a954f8
G1ck321 <118306927+G1ck321@users.noreply.github.com>
2025-11-16 16:52:11 +0100

    Update config.py

--- GDG/Test/config.py
+++ GDG/Test/config.py
@@ -5,7 +5,8 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Sampled321@localhost:5432/notes'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://googledeveloper:of1aTtbfExgy87T4Y3HeSmfob4i8syB3@dpg-d4cv3mggjchc73dlqgdg-a/notes_r71t'
 #specifies a path to the database. 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 #does not track modifications
