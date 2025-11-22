2a96a9aa9b976f7495703719fd3b6105de55abf2
G1ck321 <118306927+G1ck321@users.noreply.github.com>
2025-11-23 00:47:19 +0100

    Use environment variable for server port
    Updated app to use environment variable for port.

--- GDG/Test/app.py
+++ GDG/Test/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 from config import app
 from models import db, Note
+import os
 import datetime
 
 #status code
@@ -101,4 +102,5 @@ def deleteNotes(note_id):
     
     return jsonify('deleted'),204
 if __name__ == "__main__":
-    app.run(port=3000, debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host ="0.0.0.0",port=port debug=True)
