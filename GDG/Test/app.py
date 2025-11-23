f5a6ebbb323d30fa5257d486bb3289a20cf52a37
G1ck321 <118306927+G1ck321@users.noreply.github.com>
2025-11-23 01:04:16 +0100

    Port fixed

--- GDG/Test/app.py
+++ GDG/Test/app.py
@@ -102,5 +102,5 @@ def deleteNotes(note_id):
     
     return jsonify('deleted'),204
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT",3000))
     app.run(host ="0.0.0.0",port=port, debug=True)
