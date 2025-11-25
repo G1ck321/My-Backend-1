5ab30d4220a0d24dd4277777259da554a0f1a0c8
G1ck321 <agbejimigbemiga@gmail.com>
2025-11-25 17:30:06 +0100

    added port

--- GDG/Test/app.py
+++ GDG/Test/app.py
@@ -109,4 +109,5 @@ def deleteNotes(note_id):
     
     return jsonify(''),204
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0",port=3000, debug=True)
+    port = int(os.environ.get("PORT",3000))
+    app.run(host = "0.0.0.0",port=port, debug=True)
