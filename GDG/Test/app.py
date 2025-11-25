ea0d1ac0a2bf6ea17af4127230d91857890ec263
G1ck321 <agbejimigbemiga@gmail.com>
2025-11-25 13:26:43 +0100

    put correct URI

--- GDG/Test/app.py
+++ GDG/Test/app.py
@@ -109,4 +109,4 @@ def deleteNotes(note_id):
     
     return jsonify('deleted'),204
 if __name__ == "__main__":
-    app.run(port=3000, debug=True)
+    app.run(host = "0.0.0.0",port=3000, debug=True)
