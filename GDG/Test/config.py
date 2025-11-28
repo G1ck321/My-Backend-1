9cfb6ced1ccde18550db5b3e5006e8cbeadfe306
G1ck321 <agbejimigbemiga@gmail.com>
2025-11-28 06:43:28 +0100

    Hide URI

--- GDG/Test/config.py
+++ GDG/Test/config.py
@@ -7,14 +7,14 @@ import os
 
 load_dotenv()
 
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_USER = os.getenv('DB_USER')
-DB_NAME = os.getenv('DB_NAME')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-URI = "postgresql+psycopg2://neondb_owner:npg_f7M0DTwgPOan@ep-mute-mouse-abu9a6ai-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-# URI = "postgresql://googledeveloper:of1aTtbfExgy87T4Y3HeSmfob4i8syB3@dpg-d4cv3mggjchc73dlqgdg-a/notes_r71t"
-# URI= 'postgresql://postgres:Sampled321@localhost:5432/onboard'
+# DB_HOST = os.getenv('DB_HOST')
+# DB_PORT = os.getenv('DB_PORT')
+# DB_USER = os.getenv('DB_USER')
+# DB_NAME = os.getenv('DB_NAME')
+# DB_PASSWORD = os.getenv('DB_PASSWORD')
+URI = os.getenv('URI')
+if not URI:
+    raise RuntimeError("DATABASE_URL is not set in environment")
 app = Flask(__name__)
 CORS(app)
 
