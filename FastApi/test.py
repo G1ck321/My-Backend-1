4a5d07837252588e8f0d01daa42c41d1e2d6885b
G1ck321 <agbejimigbemiga@gmail.com>
2025-12-19 12:15:25 +0100

    JWT2

--- FastApi/test.py
+++ FastApi/test.py
@@ -1,16 +1,8 @@
 import requests
-<<<<<<< Updated upstream
-payload = {"username":"DABw","password":"newrt@4444s"}
-site= "http://127.0.0.1:3500/signup"
-#htp:// must be specifieds
-response = requests.post(url=site, json=payload)
-print(response.status_code, "code", response.text,"text")
-=======
 payload = {"title":"Deep Work","author":"Cal Newport","year":"2008"}
 site= "http://127.0.0.1:8000/books"
 #htp:// must be specifieds
 response = requests.post(url=site, json=payload)
 print(response.status_code, "code", response.text, "text")
->>>>>>> Stashed changes
 # response2 = requests.get(url = "https://google-mrwa.onrender.com/hello") 
 # print(response2.text, response2.status_code)
