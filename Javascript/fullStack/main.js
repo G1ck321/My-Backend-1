456af9774fd05692da9f5d0f23b15d59a3f9daeb
G1ck321 <agbejimigbemiga@gmail.com>
2026-05-16 15:37:16 +0100

    ngrok

--- Javascript/fullStack/main.js
+++ Javascript/fullStack/main.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const { createApp } = require('./src/app')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 const app = createApp()
 
 app.listen(port, () => {
